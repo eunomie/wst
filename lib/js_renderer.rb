c979ea8a1542493b24be810b9a2b8b3238f0e5f2
Yves Brissaud <yves@sogilis.com>
2015-01-06 14:43:08 +0100

    extract JsRenderer

---
+++ lib/js_renderer.rb
@@ -0,0 +1,59 @@
+# encoding: utf-8
+require 'configuration'
+require 'logging'
+
+module Wst
+  class JsRenderer
+    include Logging
+    include Configuration
+
+    def generate_all
+      logger.info 'Js'.blue
+      js_conf.each do |js_name|
+        logger.info "  #{js_name}"
+        compile js_name
+      end
+    end
+
+    # @param [String] js_name Name of the js to compile
+    def compile(js_name)
+      lines = read_and_expand js_name
+      js = lines.flatten.join
+      compiled = unless config['debug'] then
+                   Uglifier.compile js
+                 else
+                   js
+                 end
+      output_file = "#{config['path']}/_site/#{js_name.split('/').last}.js"
+      File.open(output_file, 'w') do |f|
+        f.write compiled
+      end
+    end
+
+    private
+    # @return [Array<String>] Array of js configurations.
+    def js_conf
+      if config.has_key?('assets') &&
+          config['assets'].has_key?('js') &&
+          config['assets']['js'].kind_of?(Array)
+        config['assets']['js']
+      else
+        []
+      end
+    end
+
+    # @param [String] js_name Name of the js file to compile
+    def read_and_expand(js_name)
+      js_file = File.join File.join(config['path'], '_js', "#{js_name}.js")
+      return [] unless File.exists? js_file
+      content = File.read js_file
+      content.lines.inject([]) do |lines, line|
+        if line =~ /\/\/=\srequire\s['"](.*)['"]/
+          lines << read_and_expand("#{$1}")
+        else
+          lines << line
+        end
+      end
+    end
+  end
+end
--- lib/wst.rb
+++ lib/wst.rb
@@ -4,6 +4,7 @@ require 'post'
 require 'page'
 require 'renderer_factory'
 require 'css_renderer'
+require 'js_renderer'
 require 'configuration'
 require 'logging'
 require 'colored'
@@ -16,6 +17,7 @@ module Wst
 
     def initialize
       @css_renderer = CssRenderer.new
+      @js_renderer = JsRenderer.new
     end
 
     # @param [Boolean] all Generate all content or only published content
@@ -38,6 +40,17 @@ module Wst
       @css_renderer.compile css_name
     end
 
+    # Compile all js files
+    def js
+      @js_renderer.generate_all
+    end
+
+    # Compile a single js file
+    # @param [String] js_name Js name to compile
+    def compile_js(js_name)
+      @js_renderer.compile js_name
+    end
+
     private
 
     def init
@@ -58,45 +71,6 @@ module Wst
     #    compile_css_expanded css_name
     #  end
     #end
-
-    def js
-      return unless config.has_key? 'assets'
-      return unless config['assets'].has_key? 'js'
-      js_conf = config['assets']['js']
-      return unless js_conf.kind_of? Array
-
-      logger.info 'Js'.blue
-      js_conf.each do |js_name|
-        logger.info "  #{js_name}"
-        compile_js js_name
-      end
-    end
-
-    # @param [String] js_name Name of the js file to compile
-    def compile_js(js_name)
-      lines = read_and_expand js_name
-      js = lines.flatten.join
-      compiled = unless config['debug'] then
-        Uglifier.compile js
-      else
-        js
-      end
-      File.open("#{config['path']}/_site/#{js_name.split('/').last}.js", 'w') { |f| f.write(compiled) }
-    end
-
-    # @param [String] js_name Name of the js file to compile
-    def read_and_expand(js_name)
-      js_file = File.join File.join(config['path'], '_js', "#{js_name}.js")
-      return [] unless File.exists? js_file
-      content = File.read js_file
-      content.lines.inject([]) do |lines, line|
-        if line =~ /\/\/=\srequire\s['"](.*)['"]/
-          lines << read_and_expand("#{$1}")
-        else
-          lines << line
-        end
-      end
-    end
 
     # @param [Boolean] all Generate all content or only published content
     def content(all)
