d932cbc26198edcaf832689c202b36040c14e81a
Yves Brissaud <yves@sogilis.com>
2015-01-06 11:36:48 +0100

    code style

--- lib/wst.rb
+++ lib/wst.rb
@@ -13,7 +13,8 @@ class Wst
   include Logging
   include Configuration
 
-  def generate all = false
+  # @param [Boolean] all Generate all content or only published content
+  def generate(all = false)
     init
     css
     js
@@ -24,66 +25,68 @@ class Wst
   private
 
   def init
-    logger.info "Clean".blue
-    FileUtils.mkdir_p File.join config['path'], "_site"
-    FileUtils.rm_rf Dir.glob File.join config['path'], "_site/*"
+    logger.info 'Clean'.blue
+    FileUtils.mkdir_p File.join config['path'], '_site'
+    FileUtils.rm_rf Dir.glob File.join config['path'], '_site/*'
   end
 
   def css
-    return unless config.has_key? "assets"
-    return unless config["assets"].has_key? "css"
-    cssconf = config["assets"]["css"]
-    return unless cssconf.kind_of? Array
+    return unless config.has_key? 'assets'
+    return unless config['assets'].has_key? 'css'
+    css_conf = config['assets']['css']
+    return unless css_conf.kind_of? Array
 
-    logger.info "Css".blue
-    cssconf.each do |cssname|
-      logger.info "  #{cssname}"
-      compile_css cssname
+    logger.info 'Css'.blue
+    css_conf.each do |css_name|
+      logger.info "  #{css_name}"
+      compile_css css_name
     end
   end
 
   def kss
-    return unless config.has_key? "assets"
-    return unless config["assets"].has_key? "css"
-    cssconf = config["assets"]["css"]
-    return unless cssconf.kind_of? Array
+    return unless config.has_key? 'assets'
+    return unless config['assets'].has_key? 'css'
+    css_conf = config['assets']['css']
+    return unless css_conf.kind_of? Array
 
-    logger.info "KSS".blue
-    cssconf.each do |cssname|
-      logger.info "  #{cssname}"
-      compile_css_expanded cssname
+    logger.info 'KSS'.blue
+    css_conf.each do |css_name|
+      logger.info "  #{css_name}"
+      compile_css_expanded css_name
     end
   end
 
   def js
-    return unless config.has_key? "assets"
-    return unless config["assets"].has_key? "js"
-    jsconf = config["assets"]["js"]
-    return unless jsconf.kind_of? Array
+    return unless config.has_key? 'assets'
+    return unless config['assets'].has_key? 'js'
+    js_conf = config['assets']['js']
+    return unless js_conf.kind_of? Array
 
-    logger.info "Js".blue
-    jsconf.each do |jsname|
-      logger.info "  #{jsname}"
-      compile_js jsname
+    logger.info 'Js'.blue
+    js_conf.each do |js_name|
+      logger.info "  #{js_name}"
+      compile_js js_name
     end
   end
 
-  def compile_js jsname
-    lines = read_and_expand jsname
+  # @param [String] js_name Name of the js file to compile
+  def compile_js(js_name)
+    lines = read_and_expand js_name
     js = lines.flatten.join
     compiled = unless config['debug'] then
       Uglifier.compile js
     else
       js
     end
-    File.open("#{config['path']}/_site/#{jsname.split('/').last}.js", "w") { |f| f.write(compiled) }
+    File.open("#{config['path']}/_site/#{js_name.split('/').last}.js", 'w') { |f| f.write(compiled) }
   end
 
-  def read_and_expand jsname
-    jsfile = File.join File.join(config['path'], '_js', "#{jsname}.js")
-    return [] unless File.exists? jsfile
-    content = File.read jsfile
-    lines = content.lines.inject([]) do |lines, line|
+  # @param [String] js_name Name of the js file to compile
+  def read_and_expand(js_name)
+    js_file = File.join File.join(config['path'], '_js', "#{js_name}.js")
+    return [] unless File.exists? js_file
+    content = File.read js_file
+    content.lines.inject([]) do |lines, line|
       if line =~ /\/\/=\srequire\s['"](.*)['"]/
         lines << read_and_expand("#{$1}")
       else
@@ -92,8 +95,9 @@ class Wst
     end
   end
 
-  def content all
-    logger.info "Content".blue
+  # @param [Boolean] all Generate all content or only published content
+  def content(all)
+    logger.info 'Content'.blue
     contents(all).each do |doc|
       logger.info "  #{doc.url}"
       renderer = RendererFactory.for doc
@@ -102,55 +106,68 @@ class Wst
   end
 
   def pub
-    logger.info "Pub".blue
+    logger.info 'Pub'.blue
 
-    outdir = File.join(config['path'], '_site')
-    indir = File.join(config['path'], '_pub', '.')
+    out_dir = File.join(config['path'], '_site')
+    in_dir = File.join(config['path'], '_pub', '.')
 
-    FileUtils.cp_r indir , outdir
-    hidden = Dir.glob("#{indir}*").select do |file|
+    FileUtils.cp_r in_dir , out_dir
+    hidden = Dir.glob("#{in_dir}*").select do |file|
       File.basename(file) != '.' && File.basename(file) != '..'
     end
-    FileUtils.cp_r hidden , outdir
+    FileUtils.cp_r hidden , out_dir
   end
 
-  def contents all
+  # @param [Boolean] all Get all content or only published content
+  # @return [Array<Content>] Contents
+  def contents(all)
     return all_content if all
     all_content.select { |c| c.published }
   end
 
+  # @return [Array<Content>] All post and page content
   def all_content
     [Post.all, Page.all].flatten
   end
 
-  def compile_css cssname
-    cssfile = get_css cssname
-    return if cssfile.nil?
+  # Compile a sass file in css.
+  # @param [String] css_name Name of the css file to create.
+  def compile_css(css_name)
+    css_file = get_css css_name
+    return if css_file.nil?
     sass_style = unless config['debug'] then
       :compressed
     else
       :expanded
     end
-    sassengine = Sass::Engine.for_file(cssfile, :syntax => sass_syntax(cssfile), :style => sass_style)
-    css = sassengine.render
+    sass_engine = Sass::Engine.for_file(css_file, :syntax => sass_syntax(css_file), :style => sass_style)
+    css = sass_engine.render
 
-    File.open("#{config['path']}/_site/#{cssname.split('/').last}.css", "w") { |f| f.write(css) }
+    File.open("#{config['path']}/_site/#{css_name.split('/').last}.css", 'w') { |f| f.write(css) }
   end
 
-  def compile_css_expanded cssname
-    cssfile = get_css cssname
-    return if cssfile.nil?
-    sassengine = Sass::Engine.for_file(cssfile, :syntax => sass_syntax(cssfile), :style => :expanded)
-    css = sassengine.render
+  # Compile a sass file in css in expanded mode.
+  # @param [String] css_name Name of the css file to create.
+  def compile_css_expanded(css_name)
+    css_file = get_css css_name
+    return if css_file.nil?
+    sass_engine = Sass::Engine.for_file(css_file, :syntax => sass_syntax(css_file), :style => :expanded)
+    css = sass_engine.render
 
-    File.open("#{config['path']}/_kss/#{cssname.split('/').last}.css", "w") { |f| f.write(css) }
+    File.open("#{config['path']}/_kss/#{css_name.split('/').last}.css", 'w') { |f| f.write(css) }
   end
 
-  def get_css cssname
-    Dir.glob(File.join(config['path'], '_css', "#{cssname}.*")).first
+  # Get the css file path.
+  # @param [String] css_name Name of the css relatively to _css directory
+  # @return [String] Name of the css relatively to _css directory
+  def get_css(css_name)
+    Dir.glob(File.join(config['path'], '_css', "#{css_name}.*")).first
   end
 
-  def sass_syntax css
+  # Get syntax (sass or scss) for a file
+  # @param [String] css Name of the file
+  # @return [symbol] :sass or :scss
+  def sass_syntax(css)
     File.extname(css).delete('.').to_sym
   end
 end
